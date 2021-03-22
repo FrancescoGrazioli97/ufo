07377bfcf4fa2f97f3f51a369dcc5be9ec913bc0
Francesco <Francesco@DESKTOP-C23CH64.wind3.hub>
2021-03-22 11:30:44 +0100

    Creazione del dao e accenno di connessione fra db e interfaccia

---
+++ src/main/java/it/polito/tdp/db/SightingDAO.java
@@ -0,0 +1,67 @@
+package it.polito.tdp.db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SightingDAO {
+
+	
+
+	
+	public List<String> readShape(){
+		try {
+		Connection conn = DBConnect.getConnection();
+		
+		String sql = "SELECT distinct shape " + 
+				"FROM sighting " + 
+				"WHERE shape<>'' " + 
+				"ORDER BY shape ASC ";
+		
+		PreparedStatement st = conn.prepareStatement(sql);
+		
+		
+		ResultSet res = st.executeQuery();
+		
+		List<String> formeUFO = new ArrayList<String>();
+		while(res.next()) {
+			
+			String forma = res.getString("shape");
+			formeUFO.add(forma);
+			
+			
+			
+		}
+		
+		st.close();
+		return formeUFO;
+		}catch(SQLException e) {
+			throw new RuntimeException("Database error in readshapes",e);
+		}
+	}
+	
+	public int countByShape(String shape) {
+		try {
+			Connection conn = DBConnect.getConnection();
+
+			String sql2 = "Select count(*) as cnt from sighting where shape= ? ";
+			
+			
+			PreparedStatement st2 = conn.prepareStatement(sql2);
+			st2.setString(1, shape);
+			ResultSet res2 = st2.executeQuery();
+			res2.first();
+			int count = res2.getInt("cnt");
+			
+			st2.close();
+			conn.close();
+			
+			return count;
+		} catch(SQLException e) {
+			throw new RuntimeException("Database error in countShape",e);
+		}
+	}
+}
